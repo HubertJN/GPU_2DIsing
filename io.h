1bcec080002b5619b9e8f5198f8c279574df6bb7
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2025-07-25 15:02:59 +0100

    Deleted old versions

--- io.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-
-void write_ising_grids(int L, int ngrids, int *ising_grids, int isweep);
-void read_input_grid(int L, int ngrids, int *ising_grids);
