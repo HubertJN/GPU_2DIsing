412aefe33a731b1b0dbf98e76880e882f9f56228
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2025-09-24 11:38:25 +0100

    Fixed variable names

--- tools/generate_trajectories.py
+++ tools/generate_trajectories.py
@@ -6,7 +6,6 @@ config = load_config("config.yaml")
 L = config.simulation.L
 ngrids = config.simulation.ngrids_factor * gasp.gpu_nsms * config.simulation.threads_per_block
 nsweeps = config.simulation.nsweeps
-nsweeps_small = config.simulation.nsweeps_small
 
 beta = config.parameters.beta
 h = config.parameters.h
@@ -17,12 +16,12 @@ mag_output_int = config.output.mag_output_int
 frac = gasp.run_nucleation_swarm(
     L,
     ngrids,
-    nsweeps_small,
+    nsweeps,
     beta,
     h,
     grid_output_int=grid_output_int,
     mag_output_int=mag_output_int,
     cv=config.collective_variable.type,
-    up_threshold=config.collective_variable.up_threshold
+    up_threshold=config.collective_variable.up_threshold,
     dn_threshold=config.collective_variable.up_threshold
 )
