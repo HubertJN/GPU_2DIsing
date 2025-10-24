dbad924233acbe7df999af8c188e8a2f54caa0e5
Hubert Naguszewski <hubert.naguszewski@warwick.ac.uk>
2025-10-24 15:51:21 +0100

    Removed manual saving, now save via gasp function

--- tools/slurm_committors.py
+++ tools/slurm_committors.py
@@ -1,10 +1,7 @@
 import os
 import argparse
-import numpy as np
-import h5py
 import gasp
 from tools.utils import load_into_array, load_config
-import gc
 
 # --- parse optional inputs ---
 parser = argparse.ArgumentParser()
@@ -16,81 +13,52 @@ args = parser.parse_args()
 # --- load config ---
 config = load_config("config.yaml")
 
-# --- apply overrides if provided ---
+# --- apply overrides ---
 beta = args.beta if args.beta is not None else config.parameters.beta
 h = args.h if args.h is not None else config.parameters.h
-dn = args.dn if args.dn is not None else config.collective_variable.dn_threshold
-
+dn_threshold = args.dn if args.dn is not None else config.collective_variable.dn_threshold
 up_threshold = config.collective_variable.up_threshold
-dn_threshold = dn
 
-# --- load dataset (path depends on beta/h) ---
+# --- load dataset ---
 training_path = f"data/gridstates_training_{beta:.3f}_{h:.3f}.hdf5"
-grids, attrs, headers = load_into_array(training_path)
+grids, _, headers = load_into_array(training_path)
 
 L = headers["L"]
-nsweeps = headers["tot_nsweeps"]
-
 gpu_nsms = gasp.gpu_nsms - gasp.gpu_nsms % config.gpu.sm_mult
 ngrids = 4 * gpu_nsms * 32
-conc_calc = min(int(np.round(ngrids / config.comm.ngrids)), gpu_nsms)
+conc_calc = min(int(round(ngrids / config.comm.ngrids)), gpu_nsms)
 
 # --- SLURM setup ---
 task_id = int(os.environ.get("SLURM_ARRAY_TASK_ID", 0))
 num_tasks = int(os.environ.get("SLURM_ARRAY_TASK_COUNT", config.gpu.tasks))
-
-task_id = task_id%num_tasks
-
 chunk_size = (len(grids) + num_tasks - 1) // num_tasks
 start_idx = task_id * chunk_size
 end_idx = min((task_id + 1) * chunk_size, len(grids))
+grids = grids[start_idx:end_idx]
+
+outpath = os.path.join(config.paths.save_dir, f"attrs_task{task_id}_{beta:.3f}_{h:.3f}.h5")
 
 print(f"[Task {task_id}] Processing grids {start_idx}:{end_idx} of {len(grids)}")
+print(f"Saving output to {outpath}")
 
-# Slice dataset
-grids = grids[start_idx:end_idx]
-attrs = attrs[start_idx:end_idx]
+# --- run committor calculation ---
+for local_start in range(0, len(grids), conc_calc):
+    local_end = min(local_start + conc_calc, len(grids))
+    gridlist = [g.copy() for g in grids[local_start:local_end]]
 
-# --- output path (depends on beta/h) ---
-outpath = os.path.join(
-    config.paths.save_dir,
-    f"attrs_task{task_id}_{beta:.3f}_{h:.3f}.h5"
-)
+    gasp.run_committor_calc(
+        L, ngrids, config.comm.nsweeps, beta, h,
+        grid_output_int=50000,
+        mag_output_int=1,
+        grid_input="NumPy",
+        grid_array=gridlist,
+        cv=config.collective_variable.type,
+        dn_threshold=dn_threshold,
+        up_threshold=up_threshold,
+        keep_grids=False,
+        nsms=gpu_nsms,
+        gpu_method=2,
+        outname=outpath
+    )
 
-total_rows = len(grids)
-ncols = attrs.shape[1]
-
-print(f"Running {conc_calc} concurrent calculation on {int(ngrids/conc_calc)} grids each")
-
-# --- main computation ---
-with h5py.File(outpath, "w") as fo:
-    dset = fo.create_dataset("attrs", shape=(total_rows, ncols), dtype=attrs.dtype)
-    offset = 0
-
-    for local_start in range(0, len(grids), conc_calc):
-        local_end = min(local_start + conc_calc, len(grids))
-        gridlist = [g.copy() for g in grids[local_start:local_end]]
-        attrlist = attrs[local_start:local_end].copy()
-
-        pBfast = np.array(
-            gasp.run_committor_calc(
-                L, ngrids, config.comm.nsweeps, beta, h,
-                grid_output_int=50000,
-                mag_output_int=1,
-                grid_input="NumPy",
-                grid_array=gridlist,
-                cv=config.collective_variable.type,
-                dn_threshold=dn_threshold,
-                up_threshold=up_threshold,
-                keep_grids=False,
-                nsms=gpu_nsms,
-                gpu_method=2,
-            )
-        )
-
-        attrlist[:, 2] = pBfast[:, 0]
-        attrlist[:, 3] = pBfast[:, 1]
-        dset[offset:offset + (local_end - local_start), :] = attrlist
-        offset += (local_end - local_start)
-
-print(f"[Task {task_id}] Finished writing {total_rows} grids to {outpath}")
+print(f"[Task {task_id}] Finished writing to {outpath}")
